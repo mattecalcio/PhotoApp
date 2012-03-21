cb82a64e76adfecaafd415fafc455b004b4430a8
Matteo Bellistri <mattecalcio@gmail.com>
2012-03-21 00:18:31 -0400

    Fixed interface for Photos page

--- PhotoAssistant/gen/cs/app/photo/R.java
+++ PhotoAssistant/gen/cs/app/photo/R.java
@@ -22,6 +22,7 @@ public final class R {
         public static final int sample_7=0x7f020008;
     }
     public static final class id {
+        public static final int ImagePreview=0x7f050006;
         public static final int button1=0x7f050001;
         public static final int button2=0x7f050002;
         public static final int button3=0x7f050003;
@@ -43,6 +44,8 @@ public final class R {
         public static final int contractsPage=0x7f040007;
         public static final int gridButton=0x7f040005;
         public static final int gridPage=0x7f040009;
+        /**   Menu  
+         */
         public static final int hello=0x7f040000;
         public static final int menu=0x7f040002;
         public static final int photosButton=0x7f040004;
--- PhotoAssistant/src/cs/app/photo/Photos.java
+++ PhotoAssistant/src/cs/app/photo/Photos.java
@@ -2,6 +2,7 @@ package cs.app.photo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +12,20 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 public class Photos extends Activity {
+	Integer[] pictures = {
+			R.drawable.sample_1,
+			R.drawable.sample_2,
+			R.drawable.sample_3,
+			R.drawable.sample_4,
+			R.drawable.sample_5,
+			R.drawable.sample_6,
+			R.drawable.sample_7
+	};
+	ImageView imageView;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -22,62 +34,62 @@ public class Photos extends Activity {
 
 		Gallery gallery = (Gallery) findViewById(R.id.gallery);
 		gallery.setAdapter(new ImageAdapter(this));
+		gallery.setSpacing(10);
 
+		imageView = (ImageView)findViewById(R.id.ImagePreview);
 		gallery.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView parent, View v, int position, long id) {
-				Toast.makeText(Photos.this, "" + position, Toast.LENGTH_SHORT).show();
+
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+				//Scaling preview picture
+				/*LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(300, 270);
+				imageView.setLayoutParams(layoutParams);*/
+				
+				imageView.setImageResource(pictures[position]);
+				
+				
 			}
 		});
 	}
-	
-	
-	//New Class
+
+
 	public class ImageAdapter extends BaseAdapter {
-	    int mGalleryItemBackground;
-	    private Context mContext;
 
-	    private Integer[] mImageIds = {
-	            R.drawable.sample_1,
-	            R.drawable.sample_2,
-	            R.drawable.sample_3,
-	            R.drawable.sample_4,
-	            R.drawable.sample_5,
-	            R.drawable.sample_6,
-	            R.drawable.sample_7
-	    };
+		private Context ctx;
+		int imageBackground;
 
-	    public ImageAdapter(Context c) {
-	        mContext = c;
-	        TypedArray attr = mContext.obtainStyledAttributes(R.styleable.HelloGallery);
-	        mGalleryItemBackground = attr.getResourceId(
-	                R.styleable.HelloGallery_android_galleryItemBackground, 0);
-	        attr.recycle();
-	    }
+		public ImageAdapter(Context c) {
+			ctx = c;
+			TypedArray ta = obtainStyledAttributes(R.styleable.HelloGallery);
+			imageBackground = ta.getResourceId(R.styleable.HelloGallery_android_galleryItemBackground, 1);
+			ta.recycle();
+		}
 
-	    public int getCount() {
-	        return mImageIds.length;
-	    }
+		public int getCount() {
+			return pictures.length;
+		}
 
-	    public Object getItem(int position) {
-	        return position;
-	    }
+		public Object getItem(int position) {
+			return position;
+		}
 
-	    public long getItemId(int position) {
-	        return position;
-	    }
+		public long getItemId(int position) {
+			return position;
+		}
 
-	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ImageView imageView = new ImageView(mContext);
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView = new ImageView(ctx);
+			
+			imageView.setImageResource(pictures[position]);
+			//imageView.setAdjustViewBounds(true);
+			imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+			imageView.setLayoutParams(new Gallery.LayoutParams(100, 70));
+			imageView.setBackgroundResource(imageBackground);
 
-	        imageView.setImageResource(mImageIds[position]);
-	        imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
-	        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-	        imageView.setBackgroundResource(mGalleryItemBackground);
-
-	        return imageView;
-	    }
+			return imageView;
+		}
 
 	}
+
 
 }
 
